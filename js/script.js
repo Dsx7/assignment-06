f72ec12b542abdc7bd52bccfd145252ac5adb4a7
Dsx7 <dsxspa@gmail.com>
2025-09-09 14:18:37 +0600

    plants and category done info from APIs

--- js/script.js
+++ js/script.js
@@ -2,15 +2,60 @@
 const getElement =(id)=> {
     return document.getElementById(id)
 }
+// Load the Category from The API
 const getCategories =()=> {
     const url = "https://openapi.programming-hero.com/api/categories"
     fetch(url)
     .then(response => response.json()) 
     .then(data => showCategories(data.categories))
 }
+// Show category
 getCategories()
 const showCategories =(categories)=> {
+    const categoriesContainer = getElement("categories-container")
+    categoriesContainer.innerHTML=""
+
     categories.forEach(element => {
-        console.log(element)
+        categoriesContainer.innerHTML += `<li>
+                <a class="px-4 py-2 duration-300 hover:bg-green-500 hover:text-white rounded-lg block" href="">${element.category_name}</a>
+              </li>
+        `
     });
+}
+
+
+// Get All Plants 
+const getPlants =(url)=> {
+    console.log(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(data => showPlants(data.plants))
+    
+}
+const getAllPlantsAPI =()=> {
+    const url = "https://openapi.programming-hero.com/api/plants"
+    getPlants(url)
+}
+getAllPlantsAPI()
+
+const showPlants =(plants)=>{
+    const plantsContainer = getElement("plants_container")
+    plantsContainer.innerHTML = ""
+    plants.forEach(item => {
+        plantsContainer.innerHTML += `
+        <div class="bg-white space-y-3 p-5 rounded-lg">
+                <img class="h-40 w-full rounded-md object-cover " src="${item.image}"
+                  alt="">
+                <h4 class="font-bold text-xl">${item.name}</h4>
+                <p class="text-[12px] text-black/50">${item.description}</p>
+                <div class="flex justify-between items-center">
+                  <p class="bg-green-100 text-green-600 px-4 rounded-full py-1">${item.category}</p>
+                  <p>৳<span>${item.price}</span></p>
+                </div>
+                <button
+                  class="bg-green-600 rounded-full w-full py-2.5 text-white cursor-pointer duration-300 border border-green-600 hover:bg-white hover:text-green-600 ">Add
+                  to Cart</button>
+              </div>
+        `
+    })
 }
